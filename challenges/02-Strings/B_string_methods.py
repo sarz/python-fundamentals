57ddfec79eff26b9ef77db6e7b2706ade14d6a70
Sarah Ismail <sarahismail@gmail.com>
2014-08-20 16:19:20 -0700

    Passed all tests for 02-Strings

--- challenges/02-Strings/B_string_methods.py
+++ challenges/02-Strings/B_string_methods.py
@@ -5,7 +5,9 @@ from A_defining_strings import your_first_name, your_last_name
 # Using string methods
 ## We're updating our administrative system, so we need to capitalize your last
 ## name. There's a simple way to do that with a string "method" called .upper()
-
+print your_last_name.upper()
+cap_last_name = your_last_name.upper()
+print cap_last_name
 ## Set cap_last_name to an uppercase version of your_last_name
 
 
@@ -17,14 +19,21 @@ from A_defining_strings import your_first_name, your_last_name
 ## And we'll need to check how long your name is to make sure there's enough
 ## space in our database! Be sure to count a space between your first and last
 ## name, and use the len() function to compute the lengths
+print your_first_name, your_last_name
+# full_name = str(your_first_name),str(your_last_name)
 
+full_name = "{} {}".format(your_first_name, your_last_name)
+print full_name
+name_len = len(full_name)
+print name_len
 ## Put the total length of your name in name_len. You can use multiple steps if
 ## you like.
 
+s = "We'll need {mylen} characters for my name"
+print s.format(mylen=len(full_name))
 
-
-print "We'll need", "your name_len here", "characters for my name"
-
+# Another way to do it:
+#print "We'll need",name_len, "characters for my name"
 
 ## Note that we've been using print with commas. This puts a space between each
 ## string or variable. But what if we don't want a space? In this case, we can
@@ -32,4 +41,6 @@ print "We'll need", "your name_len here", "characters for my name"
 ## "last name first" version of your name, with a comma separating the two.
 ## For example, I'd be "CLARK, Dav"
 
-print "CLARK, Dav... wait, that's not right!"
+print cap_last_name + ", " + your_first_name
+
+
--- challenges/02-Strings/C_interpolation.py
+++ challenges/02-Strings/C_interpolation.py
@@ -8,13 +8,19 @@
 
 s = 'this string is %d characters long'
 
+print s % len(s)
+
+# Another way to do this:
+#s2 = 'this string is {mylen} characters {mylen} long'
+#s2_len = len(s2)
+#print s2.format(mylen = s2_len)
+
 ## We can apply values to a string with an expression that goes:
 ## string % value(s)
 ## if there's more than one, put them in ()'s with commas between!
 
 ## Go ahead and compute the length of s and substitute it into the string:
 
-print 'Use the operator luke!'
 
 # conversion
 ## Adding a string and a number together doesn't make sense... for example,
@@ -23,6 +29,7 @@ print 'Use the operator luke!'
 ## use the type functions to do that, in this case, str() and float() - int()
 ## would lose the decimal point that is clearly there for a reason!
 
-print 'as "added" strings'
-
-print 'as added numbers'
+# print 'as "added" strings'
+print '1.0' + '2'
+#print 'as added numbers'
+print int(1.0) + float(2)
